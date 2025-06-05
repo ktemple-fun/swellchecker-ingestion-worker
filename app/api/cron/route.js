2aa345c1c67882d4f5257e041eb540af2ad8a925
ktemple-fun <karaanntemple@gmail.com>
2025-06-04 22:23:46 -0700

    remove tide data

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,6 +1,4 @@
 import { NextResponse } from 'next/server';
-import { fetchTideData } from '../../../src/fetchTides.js';
-import { storeTide } from '../../../src/storeTides.js';
 import { fetchCdipForecast } from '../../../src/fetchCdipForecast.js';
 import { storeCdipForecast } from '../../../src/storeCdipForecast.js';
 import spots from '../../../src/spots.js';
@@ -8,13 +6,6 @@ import spots from '../../../src/spots.js';
 export async function GET() {
   for (const spot of spots) {
  
-    try {
-      const tideData = await fetchTideData(spot.tideStationId);
-      await storeTide(spot.slug, tideData);
-    } catch (err) {
-      console.error(`Error processing tides for ${spot.slug}:`, err);
-    }
-
     try {
       const cdipData = await fetchCdipForecast();
       await storeCdipForecast(spot.slug, cdipData);
