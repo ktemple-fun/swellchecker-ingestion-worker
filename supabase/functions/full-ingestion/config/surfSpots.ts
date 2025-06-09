20d46693196fd76ab95d08d208a9526526cae534
ktemple-fun <karaanntemple@gmail.com>
2025-06-09 09:55:15 -0700

    change ingestion time to 45 min

--- supabase/functions/full-ingestion/config/surfSpots.ts
+++ supabase/functions/full-ingestion/config/surfSpots.ts
@@ -1,7 +1,30 @@
 export const surfSpots = [
-  { slug: "blacks-beach", buoy: "46254", tideStation: "9410230" },
+  // SOUTH SAN DIEGO
+  { slug: "imperial-beach", buoy: "46232", tideStation: "9410170" },
+  { slug: "coronado", buoy: "46232", tideStation: "9410170" },
+  { slug: "point-loma", buoy: "46232", tideStation: "9410170" },
+  { slug: "ocean-beach", buoy: "46232", tideStation: "9410170" },
+
+  // CENTRAL SD
+  { slug: "mission-beach", buoy: "46232", tideStation: "9410230" },
+  { slug: "pacific-beach", buoy: "46232", tideStation: "9410230" },
+
+  // LA JOLLA AREA
   { slug: "la-jolla", buoy: "46254", tideStation: "9410230" },
+  { slug: "blacks-beach", buoy: "46254", tideStation: "9410230" },
+
+  // NORTH COUNTY SD
+  { slug: "del-mar", buoy: "46254", tideStation: "9410230" },
+  { slug: "cardiff", buoy: "46254", tideStation: "9410230" },
+  { slug: "swamis", buoy: "46254", tideStation: "9410230" },
+  { slug: "encinitas", buoy: "46254", tideStation: "9410230" },
+  { slug: "carlsbad", buoy: "46225", tideStation: "9410306" },
   { slug: "oceanside", buoy: "46225", tideStation: "9410306" },
+
+  // ORANGE COUNTY (stable subset)
   { slug: "san-clemente", buoy: "46223", tideStation: "9410660" },
-  { slug: "san-diego", buoy: "46232", tideStation: "9410170" }
+  { slug: "salt-creek", buoy: "46223", tideStation: "9410660" },
+  { slug: "dana-point", buoy: "46223", tideStation: "9410660" },
+  { slug: "newport-beach", buoy: "46222", tideStation: "9410580" },
+  { slug: "huntington-beach", buoy: "46222", tideStation: "9410580" }
 ];
