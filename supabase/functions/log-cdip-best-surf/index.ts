36322ec7ee8a99a15802a216a68f3b6bee8ccbf5
ktemple-fun <karaanntemple@gmail.com>
2025-06-24 09:41:59 -0700

    remove worker

--- supabase/functions/log-cdip-best-surf/index.ts
+++
@@ -1,5 +0,0 @@
-// supabase/functions/log-cdip-best-surf/index.ts
-export default async function handler(request: Request): Promise<Response> {
-  console.log("👋 hello from log-cdip-best-surf");
-  return new Response("HELLO", { status: 200 });
-}
